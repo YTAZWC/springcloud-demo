ceb10396a9e0142ab3f9950f3bc6bbb67ec5da7e
花木凋零成兰 <18570354653@163.com>
2024-08-31 22:56:46 +0800

    hystrix-断路器 当服务提供者宕机时 将请求转发到错误提示方法

--- consumer/src/main/java/top/ytazwc/cloud/controller/UserController.java
+++ consumer/src/main/java/top/ytazwc/cloud/controller/UserController.java
@@ -1,5 +1,6 @@
 package top.ytazwc.cloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,9 +28,19 @@ public class UserController {
     private RestTemplate restTemplate;
 
     @GetMapping("/{id}")
+    @HystrixCommand(fallbackMethod = "error")
     public User findById(@PathVariable Long id) {
         // 远程调用制定地址 并传递参数 将返回结果解析为 User 实体类
         return restTemplate.getForObject(userServicePath + id, User.class);
+    }
+
+    // 响应失败时 执行的方法
+    // 提供错误信息的方法 与controller方法有同样地参数
+    public User error(Long id) {
+        User user = new User();
+        user.setId(id);
+        user.setUsername("null error");
+        return user;
     }
 
 }
